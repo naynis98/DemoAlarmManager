e698535b60c2db208c3c1153be5ec8106ecdba22
Teo Sin Yan <16033774@myrp.edu.sg>
2018-05-22 12:06:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16033774/demoalarmmanager/AlarmReceiverActivity2.java
@@ -0,0 +1,13 @@
+package com.example.a16033774.demoalarmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReceiverActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver2);
+    }
+}
